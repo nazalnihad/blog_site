999e0d7b17f6b528f4c60cc65ab39ac0d03b62ac
Nazal-nihad <nazalnihad17@gmail.com>
2023-11-10 03:42:54 +0530

    login , signup database setting up

--- website/auth.py
+++ website/auth.py
@@ -1,18 +1,26 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, redirect, url_for, request
 
 auth = Blueprint("auth", __name__)
 
 
-@auth.route("/login")
+@auth.route("/login", methods=['GET', 'POST'])
 def login():
-    return "login"
+    email = request.form.get("email")
+    password = request.form.get("password")
+
+    return render_template("login.html")
 
 
-@auth.route("/sign-up")
+@auth.route("/signup", methods=['GET', 'POST'])
 def sign_up():
-    return "sign up"
+    email = request.form.get("email")
+    username = request.form.get("username")
+    password1 = request.form.get("password1")
+    password2 = request.form.get("password2")
+
+    return render_template("signup.html")
 
 
 @auth.route("/logout")
 def logout():
-    return "logout"
+    return redirect(url_for("views.home"))
