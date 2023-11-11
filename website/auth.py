7f3cf9c7464ad93f3dfb85b7fc198c33d79362cd
Nazal-nihad <nazalnihad17@gmail.com>
2023-11-12 02:04:27 +0530

    users post creation deletion and viewing user posts done

--- website/auth.py
+++ website/auth.py
@@ -23,7 +23,7 @@ def login():
                 flash('Password is incorrect', category='error')
         else:
             flash('email not found', category='error')
-    return render_template("login.html")
+    return render_template("login.html", user=current_user)
 
 
 @auth.route("/signup", methods=['GET', 'POST'])
@@ -58,7 +58,7 @@ def sign_up():
             flash('User created')
             return redirect(url_for("views.home"))
 
-    return render_template("signup.html")
+    return render_template("signup.html", user=current_user)
 
 
 @auth.route("/logout")
