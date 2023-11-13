2a8e36b9f67aff8457b6e7d6ef3e398328a60bad
Nazal-nihad <nazalnihad17@gmail.com>
2023-11-14 02:53:49 +0530

    comments adding and deletion

--- website/views.py
+++ website/views.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, render_template, request, flash, redirect, url_for
 from flask_login import login_required, current_user
-from .models import Post, User
+from .models import Post, User, Comment
 from . import db
 
 views = Blueprint("views", __name__)
@@ -57,5 +57,41 @@ def posts(username):
         flash('Username does not exist ', category="error")
         return redirect(url_for("views.home"))
 
-    posts = Post.query.filter_by(author=user.id).all()
+    posts = user.posts
     return render_template("user_posts.html", user=current_user, posts=posts, username=username)
+
+
+@views.route("/create-comment/<post_id>", methods=['POST'])
+def create_comment(post_id):
+    text = request.form.get('text')
+
+    if not text:
+        flash('comment empty!', category='error')
+    else:
+        post = Post.query.filter_by(id=post_id).first()
+
+        if post:
+            comment = Comment(
+                text=text, author=current_user.id, post_id=post_id)
+            db.session.add(comment)
+            db.session.commit()
+        else:
+            flash('Post does not exist.', category='error')
+
+    return redirect(url_for("views.home"))
+
+
+@views.route("/delete-comment/<comment_id>")
+@login_required
+def delete_comment(comment_id):
+    comment = Comment.query.filter_by(id=comment_id).first()
+
+    if not comment:
+        flash('comment not found ', category='error')
+    elif current_user.id != comment.author and current_user.id != comment.post.author:
+        flash('you do not have permission to delete this comment', category='error')
+    else:
+        db.session.delete(comment)
+        db.session.commit()
+
+    return redirect(url_for('views.home'))
